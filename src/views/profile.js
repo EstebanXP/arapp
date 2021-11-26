94668fa3f7e280ce3735ac45326d6b515a7089ae
Efrain Estrada <a325060@uach.mx>
2021-11-25 22:01:39 -0700

    perfil basico

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import {Box, NativeBaseProvider, Heading, HStack, Text, Center, Container, Content, Flex, Badge} from "native-base"
+import React,{ useState} from 'react';
+import {
+  NativeBaseProvider,
+  Box,
+  Text,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  Icon,
+  Image,
+  HStack,
+  Center,
+  Pressable,
+} from 'native-base';
+import { View, SafeAreaView} from 'react-native'
+
 export default function profile() {
     return (
-        
-        <Box>
-            <Heading>hOLA DEsedDE PdERFIl</Heading>
-        </Box>
-            
-        
+        <View>
+             <NativeBaseProvider>
+                <Center flex={1} px="9" >
+                    <Image
+                    size={200}
+                    resizeMode={"contain"}
+                    borderRadius={100}
+                    position="absolute" top="10"
+                    source={{
+                        uri: "https://wallpaperaccess.com/full/317501.jpg",
+                    }}
+                    alt="Alternate Text"
+                    />
+                    <Heading textAlign="center" color="black" fontSize="3xl" position="absolute" top="245">
+                        name
+                    </Heading>
+                    <Text textAlign="center" color="black" position="absolute" top="72" >user</Text>
+                    <Text textAlign="center" color="black" position="absolute" top="80" >@role</Text>      
+                </Center>
+            </NativeBaseProvider>
+        </View>        
     )
 }
