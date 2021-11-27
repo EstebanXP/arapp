482bbeddd22fc4ef8dd46c33ee13a51752ffb12b
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2021-11-27 12:18:56 -0700

    Cambio en CAROUSEL

--- src/views/Carousel.js
+++ src/views/Carousel.js
@@ -8,16 +8,16 @@ const Carousel = (props) => {
     const {children, show} = props
     const [currentIndex, setCurrentIndex] = useState(0)
     const [length, setLength] = useState(children.length)
-    const [index, setIndex] =useState(4)
+    const [index, setIndex] =useState(length - show)
     const next = () => {
         if (currentIndex < (length - show)) {
-            setCurrentIndex(prevState => prevState + 1)
+            setCurrentIndex(prevState => prevState + show)
         }
     }
     
     const prev = () => {
         if (currentIndex > 0) {
-            setCurrentIndex(prevState => prevState - 1)
+            setCurrentIndex(prevState => prevState - show)
         }
     }
     // Set the length to match current children from props
@@ -51,8 +51,12 @@ const Carousel = (props) => {
             </div>
             <HStack>
             {(()=>{
+                console.log("lenght" + length);
+                console.log(show);
                 for(let i=0;i<(length - show); i++){
-                    
+                    return(
+                        <Circle mx="1"size="2" bg="indigo.500"/>
+                    )
                     
                 }
             })()}
