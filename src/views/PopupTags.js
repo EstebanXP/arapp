9390174df4a18eb0888cfeb48383cbdfc16bd1b9
Victor Aburto <victor_aburto@live.cl>
2021-12-01 17:13:43 -0300

    Uodate PopUpTags Test

--- src/views/PopupTags.js
+++ src/views/PopupTags.js
@@ -1,6 +1,8 @@
 import React,  { useEffect, useState, useRef } from 'react'
 import "../css/Popup.css";
-import {Box, Heading, Text, Badge, Button, Center, AlertDialog, HStack, Stack, View, Input} from "native-base"
+import {BsTrash, BsTrash2Fill, BsTrashFill} from "react-icons/bs"
+import {FaTimes} from 'react-icons/fa'
+
 
 import {
   collection,
@@ -13,46 +15,54 @@ import {
 } from "firebase/firestore";
 import db from "../firebase";
 
+import {Icon, Box, Heading, Text, Badge, Button, Center, AlertDialog, HStack, Stack, View, Input} from "native-base"
+
+
 const PopupTags =(props) => {
   const [newTagName,setNewTagName]= useState("")
   const [isOpen, setIsOpen] = useState(false)
   const onClose = () => setIsOpen(false)
 
   const cancelRef = useRef(null)
+  
+  const handleNTNC = (event) => {setNewTagName(event.target.value);}
 
   async function deleteTag(tagId) {
     await deleteDoc(doc(db, "Tag", tagId));
     props.setPopStatus(false);
   }
 
+  useEffect(() => {
+    console.log(props.tag)
+  }, [])
+  
   async function saveOnSubmit(e) {
     e.preventDefault();
-    const newTagName = e.target.TagTitle.value;
+    // const newTagName = e.target.TagTitle.value;
     await updateDoc(doc(db, "Tag", props.tag.id), {
       tagName: newTagName
     });
     props.setPopStatus(false);
   }
 
-  const handleNTNC = (event) => setNewTagName(event.target.value);
+ 
 
-  useEffect(() => {
-    console.log(props.tag)
-  }, [])
+ 
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
         {/*AQUI SE GUARDA LOS DATOS DE LA ROLA EN EL POPUP */}
         <Heading size="md" mb="10" textAlign="Left">{"Edit Tag"}</Heading>       
-                <button class="btn-close" onClick={() => props.setPopStatus(false)}><i class="fa fa-close"></i></button>
+                <button class="btn-close" onClick={() => props.setPopStatus(false)}><i class="fa fa-close"></i>
+        </button>
                 <br></br>
                 <br></br>
-        <form className="form-popup" onSubmit={saveOnSubmit}>
+        <form onSubmit={saveOnSubmit}>
               <HStack>
                     <Stack space={0} alignItems="left" my="auto" w="70%">
                         <HStack mb="1%" space={2} alignItems="left">
                             <View style={{justifyContent: 'center'}}>
-                                <Text fontSize="sm" w="180">Título: </Text>
+                                <Text fontSize="sm" w="100">Título: </Text>
                             </View>
                             <Input
                                 size="sm"
@@ -62,7 +72,6 @@ const PopupTags =(props) => {
                                 id="tagName"
                     
                                 placeholder="Tag Name"
-                                
                                 onChange={handleNTNC}
                                 w={"100%"}
                                 isRequired
@@ -79,26 +88,20 @@ const PopupTags =(props) => {
                   
                    
                     </HStack>
-                        <button class="btn-save" type="submit" onClick={()=>{saveOnSubmit()}}><i class="fa fa-save"></i></button>
-                        <button class="btn-delete" type="submit"
-                        onClick={() => {
-                            deleteTag(props.thisTag.id);
-                        }}
-                        ><i class="fa fa-trash"></i></button>
-          <p>Nuevo titulo: </p>
-          <input defaultValue={props.tag.tagName} name="TagTitle"></input>
-          <br></br>
-          <button type="submit">Guardar</button>
-          <Center>
-            <Button colorScheme="danger" borderRadius="10" onPress={() => setIsOpen(!isOpen)}>
-              Delete Show
+                    <br></br>     
+                    <HStack>
+                    <Stack space={0} alignItems="left" my="auto" w="100%">
+                        <HStack mb="1%" space={2} alignItems="left">                  
+                     <button class="btn-save" type="submit"><i class="fa fa-save"></i> Save Tag</button>
+            <Button colorScheme="danger" borderRadius="10" onPress={() => setIsOpen(!isOpen)} leftIcon={<BsTrashFill color="white"/>}>
+        Delete Show
             </Button>
             <AlertDialog
               leastDestructiveRef={cancelRef}
               isOpen={isOpen}
               onClose={onClose}
             >
-              <AlertDialog.Content position="absolute" top="40">
+              <AlertDialog.Content>
                 <AlertDialog.CloseButton />
                 <AlertDialog.Header>Delete Show</AlertDialog.Header>
                 <AlertDialog.Body>
@@ -123,7 +126,9 @@ const PopupTags =(props) => {
                 </AlertDialog.Footer>
               </AlertDialog.Content>
             </AlertDialog>
-          </Center>
+            </HStack>
+                    </Stack>
+                        </HStack>
         </form>
       </div>
     </div>
